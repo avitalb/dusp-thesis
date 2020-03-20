58188677157a2cd4a891c0a78b9c04e7813c835e
Avital Baral <abaral@mit.edu>
2020-03-19 23:14:48 -0400

    fixed geocoding to batches, cash plot add

--- geocoding.py
+++ geocoding.py
@@ -2,22 +2,52 @@ import pandas as pd
 import censusgeocode as cg
 from tqdm import tqdm
 from pandarallel import pandarallel
+import time
+import censusbatchgeocoder
 
 def geocode(f,seattle_acs,new_name):
     df = pd.read_csv(f)
-    df['tract'] = df.parallel_apply(lambda x : geocode_util(x['RegStNum'],x['RegStName'],x['RegStType'],x['RegCity'],x['RegState'],x['RegZipCode']), axis=1) 
+
+    df['RegStNum'] = df['RegStNum'].apply(str) 
+    print("got here")
+    df['address'] = df['RegStNum'] + " " + df['RegStName'] + " " + df['RegStType']
+    df['city'] = df['RegCity'].copy()
+    df['state'] = df['RegState'].copy()
+    df['zipcode'] = df['RegZipCode'].copy()
+    df['id'] = df.index
+
+    filtered_df = df[['address',"city","state","zipcode","id"]]
+
+    result = censusbatchgeocoder.geocode(filtered_df.to_dict("records"))
+    filtered_df= pd.DataFrame(result)
+    df = pd.merge(df,filtered_df[['address','tract']],how="left",left_on = "address",right_on = "address")
+
     print("df size before dropping null tracts",df.shape)
     df = df[df['tract'].notnull()]
     print("df size after dropping null tracts",df.shape)
 
     print("adding census tract income quintile rank for each address")
     df2 = seattle_acs
-    df = pd.merge(df,df2[['TRACT','income_quintile']],left_on='tract',right_on="tract", how='left')
 
+    # make both tract columns ints to avoid this error:  "ValueError: You are trying to merge on object and int64 columns. If you wish to proceed you should use pd.concat"
+    df["tract"] = df["tract"].astype(int)
+
+    
+    df = pd.merge(df,df2[['TRACT','income_quintile']],left_on='tract',right_on="TRACT", how='left')
 
     df.to_csv(new_name)
 
+    print("DONE")
+
+    # tqdm.pandas()
+    # df = pd.read_csv(f)
+    # df['tract'] = df.progress_apply(lambda x : geocode_util(x['RegStNum'],x['RegStName'],x['RegStType'],x['RegCity'],x['RegState'],x['RegZipCode']), axis=1) 
+    # print("df size before dropping null tracts",df.shape)
+    # df = df[df['tract'].notnull()]
+    # print("df size after dropping null tracts",df.shape)
+
 def geocode_util(num,name,typ,city,state,zipcode):
+    # time.sleep(1)
     addr = str(num) + " " + str(name) + " " + str(typ)
     try:
         result = cg.address(addr, city=city, state=state, zipcode=zipcode,returntype='geographies')
@@ -37,8 +67,16 @@ if __name__ == '__main__':
     seattle_acs = "results/seattle_acs.csv"
     df2 = pd.read_csv(seattle_acs)
 
-    print("geocode 2019")
+    print("geocode 2019 vouchers")
+    # df size before dropping null tracts (249085, 51)
+    # df size after dropping null tracts (20549, 51)
     geocode(merged_2019, df2,name_2019)
 
-    print("geocode 2017")
+    print("geocode 2017 vouchers")
     geocode(merged_2019, df2,name_2017)
+
+    print("geocode 2019 cash")
+    geocode("results/2019_cash_merged.csv", df2,"results/2019_cash_ethnicity_merged_geocoded.csv")
+
+    print("geocode 2017 cash")
+    geocode("results/2017_cash_merged.csv", df2,"results/2017_cash_ethnicity_merged_geocoded.csv")
--- plotting.py
+++ plotting.py
@@ -1,30 +1,44 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-def plot(filename,year):
+def plot(filename,year,dvp_flag):
     df = pd.read_csv(filename,low_memory = False)
-
+    title_add = ""
+    if dvp_flag:
+        title_add = 'DVP ' + str(year)
+    else:
+        tittle_add = 'Cash Contributions ' +str(year)
     # ethnicity plot
     ethn_counts = df['est_ethnicity'].value_counts()
     ethn_plot = ethn_counts.plot(kind="bar")
-    ethn_plot.set_title('Estimated Ethnicity, DVP ' + str(year))
+    ethn_plot.set_title('Estimated Ethnicity, ' + str(year))
     plt.show()
 
     # age plot
     age_counts = df['age_bucket'].value_counts()
     age_plot = age_counts.plot(kind="bar")
-    age_plot.set_title('Voucher User Age, DVP ' + str(year))
+    age_plot.set_title('Voucher User Age, ' + str(year))
     plt.show()
 
     # census tract bucket
     income_counts = df['income_bucket'].value_counts()
     income_plot = income_counts.plot(kind="bar")
-    income_plot.set_title('Voucher User Census Tract Income Quintile, DVP ' + str(year))
+    income_plot.set_title('Voucher User Census Tract Income Quintile, ' + str(year))
+    plt.show()
+
+    # gender
+    gender_counts = df['gender'].value_counts()
+    gender_plot = gender_counts.plot(kind="bar")
+    gender_plot.set_title('Voucher User Census Tract Income Quintile, ' + str(year))
     plt.show()
     
 if __name__ == '__main__':
-    # merged_2017 = 'results/2017_vouchers_voter_file_all_cols.csv'
-    merged_2019 = 'results/2019_vouchers_voter_file_all_cols.csv'
-    plot(merged_2019,2019)
-    # plot(merged_2017)
+    vouchers_2017 = 'results/2017_vouchers_voter_file_all_cols.csv'
+    vouchers_2019 = 'results/2019_vouchers_voter_file_all_cols.csv'
+    plot(vouchers_2019,2019,True)
+    plot(vouchers_2017,2017,True)
 
+    cash_2017 = 'results/2017_cash_ethnicity_merged_geocoded.csv"
+    cash_2019 = "results/2019_cash_ethnicity_merged_geocoded.csv"
+    plot(cash_2019,2019,False)
+    plot(cash_2017,2017,False)
