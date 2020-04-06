8ec00b90dc61a274291f001cec629f6125ed2878
Avital Baral <abaral@mit.edu>
2020-04-06 10:46:04 -0400

    fixed final merge columns

--- final_merge.py
+++ final_merge.py
@@ -5,9 +5,7 @@ def final_merge(geocoded,ethn,new_name):
     df1 = pd.read_csv(geocoded)
     df2 = pd.read_csv(ethn)
 
-
-    final_df = pd.merge(df1,df2[['est_ethnicity','participant_id']],on='participant_id', how='outer')
-
+    final_df = pd.merge(df1,df2[["est_ethnicity",'StateVoterID']],on='StateVoterID', how='outer')
     final_df.to_csv(new_name)
 
 if __name__ == '__main__':
@@ -22,5 +20,5 @@ if __name__ == '__main__':
     final_merge(geocoded_2019,added_ethnicity_2019,final_2019_name)
     final_merge(geocoded_2017,added_ethnicity_2017,final_2017_name)
 
-    final_merge("results/2019_cash_merged_geocoded.csv","results/2019_cash_ethnicity.csv","results/2019_cash_all_cols")
-    final_merge("results/2017_cash_merged_geocoded.csv","results/2017_cash_ethnicity.csv","results/2017_cash_all_cols")
+    # final_merge("results/2019_cash_merged_geocoded.csv","results/2019_cash_ethnicity.csv","results/2019_cash_all_cols.csv")
+    # final_merge("results/2017_cash_merged_geocoded.csv","results/2017_cash_ethnicity.csv","results/2017_cash_all_cols.csv")
